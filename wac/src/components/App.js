6b73a755af5fc971bd0cebfa62c4dc64b13b304f
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-07-15 17:24:35 +0200

    done with a few more features

--- wac/src/components/App.js
+++ wac/src/components/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Login from './Login';
+import fetch from 'node-fetch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>WAC</p>
-      </header>
-      <Login />
-    </div>
-  );
-}
+  const [geolocation, setGeolocation] = useState({});
+  const [weather, setWeather] = useState({ daily: { summary: undefined } });
+
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    setGeolocation({ lat: position.coords.latitude, long: position.coords.longitude })
+  });
+
+  fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/269bb9a5c92626a0f53367cd2c92f543/${geolocation.lat},${geolocation.long}?units=si`)
+    .then(data => data.json())
+    .then(result => {
+      if (result) setWeather(result) 
+    });
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>WAC</p>
+          </header>
+          <Login />
+        </div>
+      );
+    }
 
 export default App;
