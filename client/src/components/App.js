d5b964cd1d48b22b5d095ee9d12f4d7539b4d084
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-07-16 10:48:39 +0200

    Link express with react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import fetch from 'node-fetch';
@@ -6,26 +6,46 @@ import fetch from 'node-fetch';
 function App() {
   const [geolocation, setGeolocation] = useState({});
   const [weather, setWeather] = useState({ daily: { summary: undefined } });
+  const [apiResponse, setApiResponse] = useState({ apiRespnse: "" })
 
+  useEffect(() => {
+    console.log('this effect runs only on mount')
+    callAPI()
+  }, [])
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    setGeolocation({ lat: position.coords.latitude, long: position.coords.longitude })
-  });
+  useEffect(() => {
+    console.log('this effect runs only when apiResponse changes')
+    console.log(apiResponse)
+  }, [apiResponse])
 
-  fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/269bb9a5c92626a0f53367cd2c92f543/${geolocation.lat},${geolocation.long}?units=si`)
-    .then(data => data.json())
-    .then(result => {
-      if (result) setWeather(result) 
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => setApiResponse({ apiResponse: res }));
+  }
+
+  const getGeolocation = () => { 
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setGeolocation({ lat: position.coords.latitude, long: position.coords.longitude })
     });
+  }
 
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>WAC</p>
-          </header>
-          <Login />
-        </div>
-      );
-    }
+  const getWeather = () => {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/269bb9a5c92626a0f53367cd2c92f543/${geolocation.lat},${geolocation.long}?units=si`)
+      .then(data => data.json())
+      .then(result => {
+        if (result) setWeather(result) 
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>WAC</p>
+      </header>
+      <Login />
+    </div>
+  );
+}
 
 export default App;
