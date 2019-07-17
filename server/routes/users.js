fd4799b06d2003756d7940f9b117539216c04bcb
Sidechain <alexander.c.erni@gmail.com>
2019-07-17 11:08:37 +0200

    Check for duplicate users in database

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,33 +3,41 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017";
-
+// const getCollection = require('mongodb').getCollection;
 
 router.post('/', function (req, res, next) {
   MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
-    const dbo = db.db("mongo-mob");
-    const myobj = req.body;
-    dbo.collection("post").insert(myobj, function (err, result) {
+    const dbo = db.db('mongo-mob');
+    const user = req.body;
+    dbo.collection('users').findOne({id: user.id}, function (err, result) {
       if (err) throw err;
-      res.send("Object added to database");
+      if (!result) {
+        console.log("I am actually here also")
+        dbo.collection('users').insertOne(user, function (err, result) {
+          if (err) throw err;
+          res.send("Object added to database");
+          db.close();
+        });
+      }
+      console.log("Nothing happened")
       db.close();
-    });
+    });  
   });
 });
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-    if (err) throw err;
-    const dbo = db.db("mongo-mob");
-    dbo.collection("post").find().toArray(function (err, result) {
-      if (err) throw err;
-      res.send(result);
-      db.close();
-    });
-  });
-});
+// router.get('/', function (req, res, next) {
+//   MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+//     if (err) throw err;
+//     const dbo = db.db('mongo-mob');
+//     dbo.collection("post").find().toArray(function (err, result) {
+//       if (err) throw err;
+//       res.send(result);
+//       db.close();
+//     });
+//   });
+// });
 
 module.exports = router;
