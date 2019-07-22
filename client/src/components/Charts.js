6feda028b424804a094ed8dd6446e392a41ea477
ludvig rovira <ludvig.rovira@appliedtechnology.se>
2019-07-22 17:16:53 +0200

    First chart up and running

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -1,28 +1,18 @@
-import React, { PureComponent } from 'react';
-import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line } from 'recharts';
 
+// XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 
-const chartConfig = [
-  {
-    name: 'Page A',  pv: 23, 
-  },
-  {
-    name: 'Page B',  pv: 27, 
-  },
-  {
-    name: 'Page C',  pv: 29, 
-  },
-  {
-    name: 'Page D',  pv: 30, 
-  },
-];
-
-export default function Chart (data) {
-    console.log(data)
+export default function Chart ({data}) {
+  const [chartData, setChartData] = useState();
+  
+    useEffect( () => {
+      const forecastArray = data.weather.daily.data.map(day => ({name: day.time, pv:day.temperatureMax}));
+      setChartData(forecastArray);
+    }, [data])
+    
     return (
-      <LineChart width={300} height={100} data={chartConfig}>
+      <LineChart width={300} height={100} data={chartData}>
         <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
       </LineChart>
     );
