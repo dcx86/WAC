b1cd2cce3b7b98d0a44ed29a30ff5298017a3d51
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-07-23 17:24:30 +0200

    Change background dynamically

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Login';
 import Home from './Home';
 import ReactAnimatedWeather from 'react-animated-weather';
+import moment from 'moment';
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -14,8 +15,25 @@ function App() {
     animate: true
   };
 
+  const getClass = () => {
+    const currentTime = moment().format('HH');
+    console.log(currentTime);
+
+    switch (true) {
+      case (currentTime >= 22 && currentTime < 4): 
+        return 'App App__night'
+      case (currentTime >= 4 && currentTime < 10):
+        return 'App App__morning'
+      case (currentTime >= 10 && currentTime < 16):
+        return 'App App__day'
+      case (currentTime >= 16 && currentTime < 22):
+        return 'App App__evening'
+    }
+    return 'App'
+  }
+
   return (
-    <div className="App">
+    <div className={getClass()}>
       <header className="App__header">
         <p className="App__logo">WAC</p>
       </header>
