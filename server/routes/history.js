163d13c88789a18dd4f2182bccfb2167fe93672f
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-07-24 09:54:30 +0200

    Style charts

--- server/routes/history.js
+++ server/routes/history.js
@@ -16,10 +16,11 @@ router.post('/', function (req, res, next) {
     dbo.collection('users').find({}, {projection: {_id: 0, id:1 , history:1}}).toArray( function (err, result) {
       if (err) throw err;
       const userData = result.find(user => user.id === auth.id)
+      console.log(result)
       if (userData) {
         const history = userData.history.map(e => ({
           location: e.location.city, 
-          timeStamp: moment(e.timeStamp).format("MMM Do hh:mm"), 
+          timeStamp: moment(e.timeStamp).format("MMM Do HH:mm"), 
           aqi:e.aq.aqius,
           temp: Math.round(e.weather.currently.temperature),
           precip: e.weather.currently.precipProbability*100,
@@ -27,7 +28,7 @@ router.post('/', function (req, res, next) {
           humidity: e.weather.currently.humidity*100,
           windspeed: Math.round(e.weather.currently.windSpeed),
           visibility: Math.round(e.weather.currently.visibility),
-          co2: Math.round(e.co2.data.carbonIntensity)
+          co2: Math.round(e.co2.data)
         }));
         res.send({history})
         db.close();
